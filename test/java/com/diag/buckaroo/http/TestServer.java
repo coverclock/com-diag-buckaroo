58405cf035583e171313b02739355d4268a504d5
jsloan <jsloan@4023eed0-5898-49aa-bfa7-702407be1719>
2014-02-04 20:38:30 +0000

    Updated copyright in all modules to current form. Ported unit tests from Windows to Mac (my current development system). Improved Resource to try converting from Windows path to POSIX path. Changed port 80 to 8080 since Mac won't allow bind to 80. All unit tests pass.
    git-svn-id: svn://graphite/buckaroo/trunk/Buckaroo@113 4023eed0-5898-49aa-bfa7-702407be1719

--- test/java/com/diag/buckaroo/http/TestServer.java
+++ test/java/com/diag/buckaroo/http/TestServer.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2007 Digital Aggregates Corp., Arvada CO 80001-0597, USA.
+ * Copyright 2007-2013 Digital Aggregates Corporation, Colorado, USA.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -28,11 +28,13 @@ import java.util.logging.Logger;
 
 public class TestServer {
 
+	private int port = 8080;
+
 	@Test
 	public void test00() {
 		Server server = new Server();
 		assertNotNull(server);
-		assertEquals(server.getPort(), 80);
+		assertEquals(server.getPort(), port);
 		assertNull(server.getRoot());
 	}
 
@@ -40,27 +42,27 @@ public class TestServer {
 	public void test01() {
 		Server server = new Server();
 		assertNotNull(server);
-		assertEquals(server.getPort(), 80);
+		assertEquals(server.getPort(), port);
 		assertNull(server.getRoot());
-		Server server0 = server.setPort(8080);
+		Server server0 = server.setPort(8081);
 		assertEquals(server, server0);
-		assertEquals(server.getPort(), 8080);		
+		assertEquals(server.getPort(), 8081);		
 		Server server1 = server.setRoot("/Users/jsloan/Desktop/Home/www");
 		assertEquals(server, server1);
 		assertEquals(server.getRoot(),"/Users/jsloan/Desktop/Home/www");
 		Server server2 = server.setRoot(null);
 		assertEquals(server, server2);
 		assertNull(server.getRoot());
-		Server server3 = server.setPort(80);
+		Server server3 = server.setPort(port);
 		assertEquals(server, server3);
-		assertEquals(server.getPort(), 80);		
+		assertEquals(server.getPort(), port);		
 	}
 
 	@Test
 	public void test02() {
 		Server server = new Server();
 		assertNotNull(server);
-		assertEquals(server.getPort(), 80);
+		assertEquals(server.getPort(), port);
 		assertNull(server.getRoot());
 		Server server0 = server.start();
 		assertEquals(server, server0);
@@ -96,7 +98,7 @@ public class TestServer {
 		assertNotNull(server);
 		Logger log = server.getLogger();
 		assertNotNull(log);
-		assertEquals(server.getPort(), 80);
+		assertEquals(server.getPort(), port);
 		Server server1 = server.setRoot("/Users/jsloan/Desktop/Home/www");
 		assertEquals(server, server1);
 		assertEquals(server.getRoot(),"/Users/jsloan/Desktop/Home/www");
